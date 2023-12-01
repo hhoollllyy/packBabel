428734e2615331527a207e57c23430dbe3c22acb
hhoollllyy <zhulei20020617@126.com>
2023-12-02 05:41:16 +0800

    packBabel 7.23.5

--- dist/all.js
+++ dist/all.js
@@ -47742,22 +47742,14 @@ module.exports = function toFastproperties(o) {
 
 },{}],176:[function(require,module,exports){
 (function (global){(function (){
-/*
- * @Author       : Holly
- * @Date         : 2023-12-02 00:47:23
- * @LastEditors  : Holly
- * @LastEditTime : 2023-12-02 05:23:39
- * @Description  : 
- * @FilePath     : /src/all.js
- */
-const generate = require("@babel/generator").default;
+const generator = require("@babel/generator").default;
 const parser = require("@babel/parser");
 const template = require("@babel/template");
 const traverse = require("@babel/traverse").default;
 const types = require("@babel/types");
 let astGlb = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof globalThis !== 'undefined' ? globalThis : this;
 astGlb.babel = astGlb.babel || {};
-astGlb.babel.generate = generate;
+astGlb.babel.generator = generator;
 astGlb.babel.parser = parser;
 astGlb.babel.template = template;
 astGlb.babel.traverse = traverse;
